70290d553f9f2abe36525f2ef369611a900be6b8
khoapdaio <khoa.phamdangaio97@gmail.com>
2024-09-11 16:58:58 +0700

    refactor(app): break method, add css

--- app.py
+++ app.py
@@ -1,89 +1,126 @@
 from datetime import datetime as d
-
 import pandas as pd
 import pytz
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import re
+
+def get_person_by_email(sign_up_sheet, email):
+	"""Lấy thông tin của người dùng từ email"""
+	return sign_up_sheet[sign_up_sheet["Email"] == email]
 
 
-def main():
-	form = st.form("check_in_form")
-	form.title("Check In Form")
-	email = form.text_input("Email đăng ký")
-	submit_btn = form.form_submit_button("Submit")
-	if 'state submit' not in st.session_state:
-		st.session_state['state submit'] = True
-
-	if submit_btn:
-		conn = st.connection("gsheets", type = GSheetsConnection, ttl = 0)
-		sign_up_sheet = conn.read(worksheet = "sign_up_sheet", ttl = 0)
-		check_in_sheet = conn.read(worksheet = "check_in_sheet", ttl = 0)
-		person = sign_up_sheet[sign_up_sheet["Email"] == email]
-		if person.empty:
-			result = "Bạn chưa đăng ký"
-			has_error = True
-			check_in_person_df = pd.DataFrame(
-				[
-					{
-						"Tên": "",
-						"Email": email,
-						"Mã số đồ uống": "",
-						"Dấu thời gian": d.now().strftime("%Y-%m-%d %H:%M:%S")
-					}
-				]
-			)
-		else:
-			if person["Checked"].values[0] == 1 :
-				check_in_person_df = None
-				result = "Email này đã được dùng để check in rồi, xin hãy nhập lại email!"
-				has_error = True
-
-			else:
-				tz_VN = pytz.timezone('Asia/Ho_Chi_Minh')
-				check_in_person_df = pd.DataFrame(
-					[
-						{
-							"Tên": str(person["Tên"].values[0]),
-							"Email": str(person["Email"].values[0]),
-							"Mã số đồ uống": str(int(person["Mã số đồ uống"].values[0])),
-							"Dấu thời gian": d.now(tz_VN).strftime("%Y-%m-%d %H:%M:%S")
-						}
-					]
-				)
-				ma_do_uong = person["Mã số đồ uống"].values[0]
-				result = str(int(ma_do_uong))
-				has_error = False
+def create_check_in_person_df(name, email, drink_code, timezone='Asia/Ho_Chi_Minh'):
+	"""Tạo DataFrame thông tin check-in của người dùng"""
+	tz = pytz.timezone(timezone)
+	return pd.DataFrame(
+		[
+			{
+				"Tên": name,
+				"Email": email,
+				"Mã số đồ uống": drink_code,
+				"Dấu thời gian": d.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+			}
+		]
+	)
 
 
-		if check_in_person_df is not None:
-			with st.spinner("Waiting for it ..."):
-				update_data_check_in = pd.concat([check_in_sheet, check_in_person_df], axis = 0)
-				conn.update(worksheet = "check_in_sheet", data = update_data_check_in)
-				sign_up_sheet.loc[sign_up_sheet["Email"] == email, 'Checked'] = 1
-				conn.update(worksheet = "sign_up_sheet", data = sign_up_sheet)
-
-		st.text("submit successfully")
-		show_result(result, has_error)
-		conn.reset()
-
+def update_check_in_status(conn, email, sign_up_sheet, check_in_person_df, check_in_sheet):
+	"""Cập nhật trạng thái check-in và ghi lại thông tin"""
+	with st.spinner("Waiting for it ..."):
+		# Cập nhật dữ liệu check-in
+		updated_check_in_sheet = pd.concat([check_in_sheet, check_in_person_df], axis=0)
+		conn.update(worksheet="check_in_sheet", data=updated_check_in_sheet)
+		# Đánh dấu email đã check-in
+		sign_up_sheet.loc[sign_up_sheet["Email"] == email, 'Checked'] = 1
+		conn.update(worksheet="sign_up_sheet", data=sign_up_sheet)
+	st.text("submit successfully")
 
 @st.dialog("Mã số đồ uống")
-def show_result(result = None, isError = 0):
-	if isError:
+def show_result(result, is_error):
+	"""Hiển thị kết quả sau khi người dùng check-in"""
+	if is_error:
 		st.markdown(f"""
 					<div style="opacity:0.96;margin:auto;padding:10px 20px;">
 						<h2 style="text-align:center;">{result}</h2>
 					</div>
-					""", unsafe_allow_html = True)
+					""", unsafe_allow_html=True)
 	else:
-
 		st.markdown(f"""
 		<div style="opacity:0.96;margin:auto;padding:10px 20px;">
 			<h2 style="text-align:center;">Bạn đã check in thành công!</h2>
 			<h2 style="text-align:center;">Mã số đồ uống của bạn là:</h2>
 			<h1 style="font-family: 'Source Sans Pro', sans-serif;font-size:57px;font-weight:700;margin-bottom:25px; text-align:center;">{result}</h1>
 		</div>
-		""", unsafe_allow_html = True)
+		""", unsafe_allow_html=True)
+
+
+def main():
+	st.markdown("""
+	<style>
+	[data-testid="stAppViewContainer"]{
+	background-color: #DDDDDD;
+	}
+	
+	[data-testid="stForm"]{
+	background-color: #fff;
+	}
+	
+	</style>
+	""", unsafe_allow_html = True)
+	st.markdown("""
+				<div>
+					<h1 style="text-align:center;font-family: Poppins-Regular, sans-serif;">Check In Form</h1>
+				</div>
+			""", unsafe_allow_html = True)
+	with st.form("check_in_form",border =True ):
+
+		st.image("AIO_HN.jpg")
+		email = st.text_input("Email đăng ký")
+		st.divider()
+		st.markdown("<style>.stFormSubmitButton { display: flex; justify-content: center; }</style>", unsafe_allow_html = True)
+		submit_btn = st.form_submit_button("Submit",type="primary")
+
+	if 'state submit' not in st.session_state:
+		st.session_state['state submit'] = True
+
+	if submit_btn:
+		if email is None or email == '':
+			st.warning("Email không được để trống! Xin mời hãy nhập Email!")
+			return
+
+		if not re.match(r"^\S+@\S+\.\S+$",email):
+			st.error("Email nhập không đúng định dạng, vui lòng nhập lại Email!")
+			return
+
+		conn = st.connection("gsheets", type=GSheetsConnection, ttl=0)
+		sign_up_sheet = conn.read(worksheet="sign_up_sheet", ttl=0)
+		check_in_sheet = conn.read(worksheet="check_in_sheet", ttl=0)
+
+		person = get_person_by_email(sign_up_sheet, email)
+
+		if person.empty:
+			result = "Bạn chưa đăng ký"
+			has_error = True
+			check_in_person_df = create_check_in_person_df("", email, "")
+		else:
+			if person["Checked"].values[0] == 1:
+				check_in_person_df = None
+				result = "Email này đã được dùng để check in rồi, xin hãy nhập lại email!"
+				has_error = True
+			else:
+				name = person["Tên"].values[0]
+				email = person["Email"].values[0]
+				drink_code = str(int(person["Mã số đồ uống"].values[0]))
+				check_in_person_df = create_check_in_person_df(name, email, drink_code)
+				result = drink_code
+				has_error = False
+
+		if check_in_person_df is not None:
+			update_check_in_status(conn, email, sign_up_sheet, check_in_person_df, check_in_sheet)
+
+		show_result(result, has_error)
+		conn.reset()
 
 
 if __name__ == '__main__':
